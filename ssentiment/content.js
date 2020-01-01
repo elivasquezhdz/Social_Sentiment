475c5921997f8f0159c9431e236629bf7fbe9819
Eli Vasquez <eli.vasquezhdz@gmail.com>
2019-12-31 21:18:02 -0800

    Update content.js

--- ssentiment/content.js
+++ ssentiment/content.js
@@ -2,23 +2,16 @@ console.log("wazaaaaaaaa!!!");
 console.log("Starting Social Sentiment");
 console.log("Wazaaaaaaaa!!!!");
 var elements = document.getElementsByClassName("style-scope ytd-comment-renderer");
-var names = '';
 const threshold = 0.9;
-var sentences = []; 
 for(var i=0; i<elements.length; i++) {
     console.log(elements[i].innerText);
-    sentences.push(elements[i].innerText);
-    //elements[i].style['background-color'] = "#FF6961";
-
-    model.classify(sentences).then(predictions => {elements[i].innerText});
-    for(let p=0; p<7;p++)
-    {
-        if(predictions[p].results[0].match===true)
+    model.classify(elements[i].innerText).then(predictions => {
+        for(let p=0; p<7;p++)
         {
-            elements[i].style['background-color'] = "#FF6961";
-
+            if(predictions[p].results[0].match===true)
+            {
+                elements[i].style['background-color'] = "#FF6961";  
+            }
         }
-
-    }
-
+        });
 }
